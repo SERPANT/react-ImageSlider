7cc599c6cbd6e514c0bea8784e351d2e548922c8
Shreejit <shreejit955@gmail.com>
2018-11-09 21:29:20 +0545

    Updates

---
+++ precache-manifest.f13a53988255bbd43b629f84b6c106f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c4261415db8c6db10e0",
+    "url": "/react-ImageSlider/static/js/runtime~main.4c426141.js"
+  },
+  {
+    "revision": "21cd27726a4d5f492658",
+    "url": "/react-ImageSlider/static/js/main.21cd2772.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/react-ImageSlider/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "21cd27726a4d5f492658",
+    "url": "/react-ImageSlider/static/css/main.ff6c3613.chunk.css"
+  },
+  {
+    "revision": "b89f920dd26f2c89847ee25beff7d09a",
+    "url": "/react-ImageSlider/index.html"
+  }
+];
